755f06c0ebbad25a8a3354f63309a75f336198e9
Nicholas-Vellios <nicholas.vellios@students.mq.edu.au>
2021-10-13 11:50:19 +1100

    Added hills, a cliff and various 3D models from the nature pack.

--- COMP2160 Assignment 3/Assets/Scripts/Drive.cs
+++ COMP2160 Assignment 3/Assets/Scripts/Drive.cs
@@ -26,7 +26,7 @@ public class Drive : MonoBehaviour
             movement = new Vector3(0, 0, Input.GetAxis("Vertical"));
             steering = new Vector3(0, Input.GetAxis("Horizontal") * Input.GetAxis("Vertical"), 0);
             carBody.AddTorque(steering * steerSpeed * steerAccel);
-            carBody.AddRelativeForce(movement * driveSpeed * driveAccel);
+            carBody.AddRelativeForce(movement * driveSpeed * driveAccel, ForceMode.Acceleration);
         }
     }
 
